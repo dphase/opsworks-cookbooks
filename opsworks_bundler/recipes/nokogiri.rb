1cd08a4cccbf53b887517e38f5a86edfba399af9
dphase <joshua.deere@gmail.com>
2014-08-02 14:26:27 -0500

    by force

--- opsworks_bundler/recipes/nokogiri.rb
+++ opsworks_bundler/recipes/nokogiri.rb
@@ -1,6 +1,8 @@
 if node[:opsworks_bunder][:manage_package]
   ruby_block "Configuring Bundler to use system libs for Nokogiri" do
     block do
+      system("echo 'NOKOGIRI_USE_SYSTEM_LIBRARIES=true' >> /etc/profile")
+      system("echo 'NOKOGIRI_USE_SYSTEM_LIBRARIES=true' >> /etc/environment")
       system("sudo su - deploy -c /usr/local/bin/bundle config build.nokogiri --use-system-libraries")
     end
   end
