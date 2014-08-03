95b87ae4b2b4b8b878c6dae28342f55b12752b29
dphase <joshua.deere@gmail.com>
2014-08-02 20:36:33 -0500

    hmmm

--- unicorn/metadata.rb
+++ unicorn/metadata.rb
@@ -1,7 +1,5 @@
-name        "ss-opsworks-overrides"
+name        "unicorn"
 description "Override Opsworks defaults"
 maintainer  "SchoolStatus"
 license     "Apache 2.0"
 version     "1.0.0"
-
-depends 'rails'
