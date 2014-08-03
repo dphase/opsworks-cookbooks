b21b115bcab2df36a5d6ed551d8bcf0993e00105
dphase <joshua.deere@gmail.com>
2014-08-02 20:00:08 -0500

    try this

--- unicorn/metadata.rb
+++ unicorn/metadata.rb
@@ -1,6 +1,6 @@
-name        "unicorn"
-description "Manage unicorn"
-maintainer  "AWS OpsWorks"
+name        "ss-opsworks-overrides"
+description "Override Opsworks defaults"
+maintainer  "SchoolStatus"
 license     "Apache 2.0"
 version     "1.0.0"
 
